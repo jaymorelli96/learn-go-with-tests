8c562f728e9fb1d185e10d187504ffa8f13300ea
Jean Morelli <jcrmorelli@gmail.com>
2023-03-13 16:33:37 +0000

    support for empty string in Hello

---
+++ hello-world/hello.go
@@ -0,0 +1,14 @@
+package main
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
+}
+
+func main() {
+
+}
